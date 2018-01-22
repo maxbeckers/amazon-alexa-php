21b2bccd16621b09b8e179433843219bce358834
Maximilian Beckers <beckers.maximilian@gmail.com>
2018-01-22 11:25:09 +0100

    AudioPlayer Response fixes

--- src/Response/Directives/AudioPlayer/AudioItem.php
+++ src/Response/Directives/AudioPlayer/AudioItem.php
@@ -11,4 +11,13 @@ class AudioItem
      * @var Stream
      */
     public $stream;
+
+    public static function create(Stream $steam)
+    {
+        $audioItem = new self();
+
+        $audioItem->stream = $steam;
+
+        return $audioItem;
+    }
 }
