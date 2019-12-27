cf60721203fe9cbac27bc1e84f6eb80cec3354fa
Salvatore Cordiano <salvatore.cordiano@facile.it>
2019-12-27 19:24:25 +0100

    After @maxbeckers review

--- src/Response/Directives/Dialog/Entity/TypeValue.php
+++ src/Response/Directives/Dialog/Entity/TypeValue.php
@@ -38,14 +38,7 @@ class TypeValue implements \JsonSerializable
     }
 
     /**
-     * Specify data which should be serialized to JSON.
-     *
-     * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
-     *
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     *               which is a value of any type other than a resource
-     *
-     * @since 5.4.0
+     * {@inheritdoc}
      */
     public function jsonSerialize()
     {
